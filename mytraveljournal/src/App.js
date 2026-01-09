c1ff8f6cd5e3f1684108f9b1f853b5decd032e27
Umesh Jagtap <umeshjagtap.7866@gmail.com>
2026-01-09 11:29:48 +0530

    mytraveljournal
    mytraveljournal

---
+++ mytraveljournal/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import TravelJournal from './components/TravelJournal';
+
+function App() {
+  return (
+    <div className="App">
+      <TravelJournal />
+    </div>
+  );
+}
+
+export default App;
---
+++ mytraveljournal/src/data.js
@@ -0,0 +1,43 @@
+// Requirements :
+// title, location, Google Maps link, start Date, end Date, description, image URL
+
+const data = [
+  {
+    id: 1,
+    title: 'Sinhgad',
+    location: 'Maharashtra, India',
+    googleMapsUrl: 'https://goo.gl/maps/CJTPsvJPCC9jjCQLA',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Sinhagad is an ancient hill fortress located at around 49 km southwest of the city of Pune, India. Sinhagad. Pune District, Maharashtra, India. ',
+    imageURL: 'https://i.postimg.cc/fRgjmWgw/shutterstock-1180915588.jpg',
+  },
+  {
+    id: 2,
+    title: 'Ghoradeshwar',
+    location: 'Maharashtra, India',
+    googleMapsUrl: 'https://goo.gl/maps/Fbm7W8bKcSAtpKW16',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Ghoradeshwar Hill is famous for the ancient temple of Lord Shiva and Buddhist caves. There are two temples of Lord Shiva. I was very excited about this trekking and felt very thrilled.',
+    imageURL:
+      'https://th.bing.com/th/id/R.a6b535ccf5ba0a12de48bfc1edd75f8e?rik=s%2b7eEOiiIHmy0Q&riu=http%3a%2f%2fwallup.net%2fwp-content%2fuploads%2f2016%2f03%2f10%2f343202-landscape-nature.jpg&ehk=Qo5%2bvQJS33LvXkoVX%2fjxqNjgO0eY2dQg3HVk8otjvnc%3d&risl=1&pid=ImgRaw&r=0',
+  },
+  {
+    id: 3,
+    title: 'Malvan',
+    location: 'Maharashtra, India',
+    googleMapsUrl: 'https://goo.gl/maps/mR1jeszkEsvWQnbx5',
+    startDate: '12 Jan, 2021',
+    endDate: '24 Jan, 2021',
+    description:
+      'Stretching from Achara in the North to Devbaug in the South, the beaches in Malvan are synonymous with beauty, peace and serenity. The extensive coastline gives rise to a number of beaches with lengthy shores ideal for long romantic walks amidst turquoise blue waters and shiny silver sands.',
+    imageURL:
+      'https://th.bing.com/th?id=OIP.MBlOJPg-beF5E6q2yR5k9gAAAA&w=204&h=306&c=8&rs=1&qlt=90&o=6&dpr=1.4&pid=3.1&rm=2',
+  },
+];
+
+// https://th.bing.com/th/id/OIP.PK4td4vOkvNmJJ41hPIGtQHaFj?w=247&h=185&c=7&r=0&o=5&dpr=1.4&pid=1.7
+export default data;
